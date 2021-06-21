4a40102debf660fc161f175fd091e93de93cbc35
Ishika <ishukiki2002@gmail.com>
2021-06-21 23:41:54 +0530

    new files added

---
+++ public/script.js
@@ -0,0 +1,61 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+
+const peer =  new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+
+const localVideo = document.createElement('video')
+localVideo.muted = true
+const peers = {}
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+    addVideoStream(localVideo, stream)
+
+    peer.on('call', call => {
+        call.answer(stream)
+        const video = document.createElement('video')
+        call.on('stream', userVideoStream => {
+            addVideoStream(video, userVideoStream)
+        })
+    })
+
+    socket.on('user-connected', userId => {
+        connectToNewUser(userId, stream)
+        console.log('user connected: '+ userId)
+    })
+})
+
+socket.on('user-disconnected', userId => {
+    if(peers[userId]) peers[userId].close()
+    console.log('user disconnected: ', userId)
+})
+
+peer.on('open', id => {
+    socket.emit('join-room', room_id, id)
+})
+
+function connectToNewUser(userId, stream){
+    const call = peer.call(userId, stream)
+    const video = document.createElement('video')
+    call.on('stream', userVideoStream => {
+        addVideoStream(video, userVideoStream)
+    })
+    call.on('close', () => {
+        video.remove()
+    })
+
+    peers[userId] = call
+}
+
+function addVideoStream(video, stream){
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
